81c49b1cbebd15bac20e7593b6f37833e0c20785
SksOp <shubhaman47@gmail.com>
2024-03-17 20:00:31 +0530

    feat: Refactor layout and add animation to home page

--- components/section/home/animated-gif.tsx
+++ components/section/home/animated-gif.tsx
@@ -4,30 +4,42 @@ import Image from "next/image";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Animatedgif() {
   const containerRef = useRef(null);
   const imageRef = useRef(null);
   useGSAP(
     () => {
-      gsap.from(imageRef.current, {
-        y: 1000,
-        duration: 2,
-        ease: "power4.out",
-        opacity: 0,
-      });
+      if (!containerRef.current || !imageRef.current) return;
+      gsap.fromTo(
+        imageRef.current,
+        {
+          y: 500,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          ease: "back.out(1)",
+        },
+      );
     },
-    { scope: containerRef, dependencies: [] },
+
+    { scope: containerRef },
   );
   return (
-    <div ref={containerRef} className=" w-full lg:relative lg:h-[70vh] ">
+    <div ref={containerRef} className="mb-10 w-full lg:relative lg:h-[70vh] ">
       <Image
         ref={imageRef}
         src="/image/nf-demo.png"
         alt="Hero"
         width={1920}
         height={1080}
-        className="w-auto -translate-y-14 object-contain lg:absolute lg:bottom-20 lg:left-1/2 lg:h-full lg:-translate-x-1/2 lg:translate-y-0 lg:scale-110"
+        className="w-auto  -translate-y-14 object-contain opacity-0 lg:absolute lg:bottom-20 lg:left-1/2 lg:h-full lg:-translate-x-1/2 lg:translate-y-0 lg:scale-110"
       />
     </div>
   );
